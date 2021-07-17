602252d97d589786b651ed8aa08cecc9cba5315a
Ángel M <angelmmg90>
2021-07-17 14:06:47 +0200

    Initial commit

---
+++ autoScrollContent/src/main/java/com/teseo/studios/autoscrollcontent/AutoScrollContent.kt
@@ -0,0 +1,13 @@
+package com.teseo.studios.autoscrollcontent
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.recyclerview.widget.RecyclerView
+
+class AutoScrollContent @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : RecyclerView(context, attrs, defStyle) {
+
+}
